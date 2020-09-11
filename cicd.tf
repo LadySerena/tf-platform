7b1323ebe2bf1ca70b0d2757fa0bc9c326df748e
LadySerena <serena.tiede+github@gmail.com>
2020-09-10 19:32:08 -0500

    feat: add base image triggers

--- cicd.tf
+++ cicd.tf
@@ -6,3 +6,12 @@ module "platform-terraform-triggers" {
   ciMainPath = "ci/apply/cloudbuild.yaml"
   ciDevPath = "ci/plan/cloudbuild.yaml"
 }
+
+module "base-image-triggers" {
+  source = "git::https://github.com/LadySerena/terraform-modules.git//pr-and-tag?ref=0.2.0"
+  ownerName = "LadySerena"
+  repoName = "gce-base-image"
+  project_id = data.google_project.project.project_id
+  ciMainPath = "ci/release/cloudbuild.yaml"
+  ciDevPath = "ci/feature/cloudbuild.yaml"
+}
