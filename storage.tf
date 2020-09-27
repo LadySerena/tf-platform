3ba85873bae4dee9892b4e90e4cdf9162dedea4d
LadySerena <serena.tiede+github@gmail.com>
2020-09-26 21:06:52 -0500

    fix: use project_id instead of id
    This caused some issues with terraform working for the cloud storage
    bucket.

--- storage.tf
+++ storage.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "debian-v1" {
   name = "debian-v1.platform.serenacodes.com"
   location = "US"
-  project = data.google_project.project.id
+  project = data.google_project.project.project_id
   storage_class = "STANDARD"
   uniform_bucket_level_access = true
 }
