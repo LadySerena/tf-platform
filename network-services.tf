211d1b1865da4e405295544a9b360fadb4adecb4
LadySerena <serena.tiede+github@gmail.com>
2020-10-10 22:49:45 -0500

    fix: move dns zone to platform project

--- network-services.tf
+++ network-services.tf
@@ -1,1 +1,6 @@
+resource "google_dns_managed_zone" "default" {
+  dns_name    = "serenacodes.com."
+  name        = "serenacodes-zone"
+  description = "dns zone for serenacodes.com"
+}
 
--- provider.tf
+++ provider.tf
@@ -1,12 +1,12 @@
 provider "google" {
   project = "telvanni-platform"
-  region = "us-central1"
+  region  = "us-central1"
   version = "~> 3.34"
-  zone = "us-central1-c"
+  zone    = "us-central1-c"
 }
 provider "google-beta" {
   project = "telvanni-platform"
-  region = "us-central1"
-  zone = "us-central1-c"
+  region  = "us-central1"
+  zone    = "us-central1-c"
   version = "~> 3.35"
 }
