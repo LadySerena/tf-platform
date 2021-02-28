43472c36b6df037a41a50cc47dc53ada038dbfa2
LadySerena <serena.tiede+github@gmail.com>
2021-02-28 16:50:27 -0600

    feat: remove sudo since script runs as root

--- scripts/minecraft-init.sh
+++ scripts/minecraft-init.sh
@@ -8,29 +8,29 @@ volume_group_name=$(curl http://metadata.google.internal/computeMetadata/v1/inst
 lvm_name=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/lvm-name -H "Metadata-Flavor: Google")
 
 create_lvm() {
-  sudo pvcreate "/dev/disk/by-id/google-$disk_id"
-  sudo vgcreate "$volume_group_name" "/dev/disk/by-id/google-$disk_id"
-  sudo lvcreate -l +100%FREE "$volume_group_name" -n "$lvm_name"
+  pvcreate "/dev/disk/by-id/google-$disk_id"
+  vgcreate "$volume_group_name" "/dev/disk/by-id/google-$disk_id"
+  lvcreate -l +100%FREE "$volume_group_name" -n "$lvm_name"
 }
 
 create_fs() {
-  sudo mkfs.ext4 "/dev/$volume_group_name/$lvm_name"
+  mkfs.ext4 "/dev/$volume_group_name/$lvm_name"
 }
 
 mount_lvm() {
-  sudo mount "/dev/$volume_group_name/$lvm_name" "$mount_point"
+  mount "/dev/$volume_group_name/$lvm_name" "$mount_point"
 }
 
 if [ ! -d "$mount_point" ]; then
   echo "$mount_point doesn't exist creating it now"
-  sudo mkdir -p "$mount_point"
+  mkdir -p "$mount_point"
 
 else
   echo "$mount_point exists skipping mkdir"
 fi
 
 echo "setting ownership of $mount_point to $owner"
-sudo chown -R "$owner" "$mount_point"
+chown -R "$owner" "$mount_point"
 
 if [ ! -d "/dev/$volume_group_name" ]; then
   echo "lvm doesn't exist now creating"
@@ -40,14 +40,14 @@ else
 fi
 
 # check if disk has been formatted if it hasn't then format it with ext4 filesystem
-if ! sudo file -sL /dev/$volume_group_name/$lvm_name | grep ext4 >>/dev/null; then
+if ! file -sL "/dev/$volume_group_name/$lvm_name" | grep ext4 >>/dev/null; then
   echo "creating filesystem"
   create_fs
 else
   echo "disk is formatted skipping filesystem creation"
 fi
 
-if findmnt -M $mount_point >>/dev/null; then
+if findmnt -M "$mount_point" >>/dev/null; then
   echo "lvm is already mounted skipping mounting"
 else
   echo "mounting lvm"
