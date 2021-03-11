d1b7effe10e94197a784f0ddb2e1233a944d5973
LadySerena <serena.tiede+github@gmail.com>
2021-03-10 19:07:10 -0600

    fix: comment out pubsub for now

--- data.tf
+++ data.tf
@@ -1,19 +1,19 @@
-data "google_cloudfunctions_function" "discord_notifier" {
-  name = "discord-notifier"
-}
-
-module "cloud_build_pub_sub" {
-  source  = "terraform-google-modules/pubsub/google"
-  version = "~> 1.9"
-
-  topic      = "cloud-builds"
-  project_id = data.google_project.project.project_id
-  push_subscriptions = [
-    {
-      name                    = "push"
-      ack_deadline_seconds    = 20
-      push_endpoint           = data.google_cloudfunctions_function.discord_notifier.https_trigger_url
-      enable_message_ordering = true
-    }
-  ]
-}
+#data "google_cloudfunctions_function" "discord_notifier" {
+#  name = "discord-notifier"
+#}
+#
+#module "cloud_build_pub_sub" {
+#  source  = "terraform-google-modules/pubsub/google"
+#  version = "~> 1.9"
+#
+#  topic      = "cloud-builds"
+#  project_id = data.google_project.project.project_id
+#  push_subscriptions = [
+#    {
+#      name                    = "push"
+#      ack_deadline_seconds    = 20
+#      push_endpoint           = data.google_cloudfunctions_function.discord_notifier.https_trigger_url
+#      enable_message_ordering = true
+#    }
+#  ]
+#}
