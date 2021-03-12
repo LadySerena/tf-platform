851acb2587ac6f9bdbb448ce16d579ab0e1023a4
LadySerena <serena.tiede+github@gmail.com>
2021-03-11 20:13:52 -0600

    fix: remove oidc email

--- data.tf
+++ data.tf
@@ -10,11 +10,10 @@ module "cloud_build_pub_sub" {
   project_id = data.google_project.project.project_id
   push_subscriptions = [
     {
-      name                       = "push"
-      ack_deadline_seconds       = 20
-      push_endpoint              = data.google_cloudfunctions_function.discord_notifier.https_trigger_url
-      enable_message_ordering    = true
-      oidc_service_account_email = module.project-services.enabled_api_identities["pubsub.googleapis.com"]
+      name                    = "push"
+      ack_deadline_seconds    = 20
+      push_endpoint           = data.google_cloudfunctions_function.discord_notifier.https_trigger_url
+      enable_message_ordering = true
     }
   ]
 }
